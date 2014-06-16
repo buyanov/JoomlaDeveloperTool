e9e9892df39c32e15eff358c8e8b58482835b505
Redknife <redknife183@gmail.com>
2014-06-17 00:26:22 +0600

    Fixed small bug

--- jtools/jcomponent.py
+++ jtools/jcomponent.py
@@ -56,6 +56,7 @@ def create_component(name, author, description, ToolConf):
         shutil.copytree(src_dir, dst_dir)
     except Exception as e:
         print(e)
+    os.chdir(dst_dir)
     utils.remake_dir(os.curdir, vars_dict)
 
 def create_table(name, author, ToolConf):
